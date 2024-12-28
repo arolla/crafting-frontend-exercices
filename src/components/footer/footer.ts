8a66a142ff8882f69e56d36c8d89996e24c0f776
Olivier Penhoat <openhoat@gmail.com>
2024-12-28 08:42:28 +0100

    Exercise2 - solution

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -1,12 +1,43 @@
-import { WebComponent } from '@util/web-component'
+import { html } from 'common-tags'
+import css from './footer.scss?inline'
 
-export default class Footer extends WebComponent {
+import { staticImplements } from '@util/decorators.helper.'
+import { WebComponent, type WebComponentConstructor } from '@util/web-component'
+
+@staticImplements<WebComponentConstructor>()
+export class Footer extends WebComponent {
   static register() {
     WebComponent.register('arl-footer', Footer)
   }
 
-  // TODO
-  buildTemplate(): string {
-    throw new Error('Method not implemented.')
+  static get observedAttributes() {
+    return ['is-user-logged-in']
+  }
+
+  userLoggedIn = false
+
+  get text(): string {
+    return `Contact | Map | ${this.userLoggedIn ? 'Log out' : 'Log in'}`
+  }
+
+  buildTemplate() {
+    return html`
+      <style>${css}</style>
+      <div id="footer">
+        <h3>Need help?</h3>
+        <p>${this.text}</p>
+      </div>
+    `
+  }
+
+  attributeChangedCallback(
+    attributeTagName: string,
+    _formerValue: string,
+    newValue: string,
+  ) {
+    if (attributeTagName === 'is-user-logged-in') {
+      this.userLoggedIn = newValue === 'true'
+      this.render()
+    }
   }
 }
