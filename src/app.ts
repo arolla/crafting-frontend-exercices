d1e6de7e27fbb6d2dc555f2dd7aa30075bc10895
Olivier Penhoat <openhoat@gmail.com>
2024-12-28 08:42:28 +0100

    Exercise2 - solution

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { Footer } from '@components/footer/footer'
 import { Header } from '@components/header/header'
 import { staticImplements } from '@util/decorators.helper.'
 import { WebComponent, type WebComponentConstructor } from '@util/web-component'
@@ -9,6 +10,7 @@ export class App extends WebComponent {
   static register() {
     WebComponent.register('arl-app', App)
     Header.register()
+    Footer.register()
   }
 
   buildTemplate() {
@@ -18,6 +20,7 @@ export class App extends WebComponent {
       </style>
       <div class="app">
         <arl-header></arl-header>
+        <arl-footer is-user-logged-in="true"></arl-footer>
       </div>
     `
   }
