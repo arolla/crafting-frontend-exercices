c96648f47637659c55509242e9d76bcc3c12aa59
Olivier Penhoat <openhoat@gmail.com>
2025-06-02 15:27:23 +0200

    Exercise 5 - solution

--- test/e2e/betting-page.e2e.test.ts
+++ test/e2e/betting-page.e2e.test.ts
@@ -26,5 +26,75 @@ test.describe(
       // Then
       await bettingPage.expectPageLoaded()
     })
+    test('should render stake given the user clicks on "team2"', async ({
+      page,
+    }) => {
+      // Given
+      await bettingPage.expectPageLoaded()
+      // When
+      await bettingPage.clickTeamButton('team2')
+      // Then
+      await bettingPage.expectStake({ visible: true })
+    })
+    test('should render summary given the user sets stake', async ({
+      page,
+    }) => {
+      // Given
+      await bettingPage.expectPageLoaded()
+      await bettingPage.clickTeamButton('team2')
+      await bettingPage.expectStake({ visible: true })
+      // When
+      await bettingPage.setStake(100)
+      // Then
+      await bettingPage.expectSummary({
+        text: 'Number of bets placed: 1 Potential gain: 50 €',
+        visible: true,
+      })
+    })
+    test('should update summary given the user clicks on "team3"', async ({
+      page,
+    }) => {
+      // Given
+      await bettingPage.expectPageLoaded()
+      await bettingPage.clickTeamButton('team2')
+      await bettingPage.expectStake({ visible: true })
+      await bettingPage.setStake(100)
+      await bettingPage.expectSummary({
+        text: 'Number of bets placed: 1 Potential gain: 50 €',
+        visible: true,
+      })
+      // When
+      await bettingPage.clickTeamButton('team3')
+      // Then
+      await bettingPage.expectSummary({
+        text: 'Number of bets placed: 2 Potential gain: 114 €',
+        visible: true,
+      })
+    })
+    test('should update summary given the user change stake', async ({
+      page,
+    }) => {
+      // Given
+      await bettingPage.expectPageLoaded()
+      await bettingPage.clickTeamButton('team2')
+      await bettingPage.expectStake({ visible: true })
+      await bettingPage.setStake(100)
+      await bettingPage.expectSummary({
+        text: 'Number of bets placed: 1 Potential gain: 50 €',
+        visible: true,
+      })
+      await bettingPage.clickTeamButton('team3')
+      await bettingPage.expectSummary({
+        text: 'Number of bets placed: 2 Potential gain: 114 €',
+        visible: true,
+      })
+      // When
+      await bettingPage.setStake(200)
+      // Then
+      await bettingPage.expectSummary({
+        text: 'Number of bets placed: 2 Potential gain: 229 €',
+        visible: true,
+      })
+    })
   },
 )
